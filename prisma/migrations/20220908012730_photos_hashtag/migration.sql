0b917ea7540c53c8614e97faea4552951c1ae63c
Jung Su <wjdwjdtn92@naver.com>
2022-09-10 10:22:49 +0900

    feat: add see rooms api

---
+++ prisma/migrations/20220908012730_photos_hashtag/migration.sql
@@ -0,0 +1,51 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userId` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "userId";
+
+-- CreateTable
+CREATE TABLE "Photo" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "file" TEXT NOT NULL,
+    "caption" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Photo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Hashtag" (
+    "id" SERIAL NOT NULL,
+    "hastag" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Hashtag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_HashtagToPhoto" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_HashtagToPhoto_AB_unique" ON "_HashtagToPhoto"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_HashtagToPhoto_B_index" ON "_HashtagToPhoto"("B");
+
+-- AddForeignKey
+ALTER TABLE "Photo" ADD CONSTRAINT "Photo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_HashtagToPhoto" ADD CONSTRAINT "_HashtagToPhoto_A_fkey" FOREIGN KEY ("A") REFERENCES "Hashtag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_HashtagToPhoto" ADD CONSTRAINT "_HashtagToPhoto_B_fkey" FOREIGN KEY ("B") REFERENCES "Photo"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ users/seeFollowers copy/seeFollowers.resolvers.js
@@ -0,0 +1,34 @@
+import client from "../../../client";
+
+const showFollowersCount = 5;
+
+export default {
+    Query: {
+        seeFollowers: async (_, { username, page }) => {
+            const ok = await client.user.findUnique({
+                where: { username },
+                select: { id: ture },
+            });
+            if (!ok) {
+                return {
+                    ok: false,
+                    error: "User not found",
+                };
+            }
+            const followers = await client.user
+                .findUnique({ where: { username } })
+                .followers({
+                    take: showFollowersCount,
+                    skip: (page - 1) * showFollowersCount,
+                });
+            const totalFollowers = await client.user.count({
+                where: { following: { some: { username } } },
+            });
+            return {
+                ok: true,
+                followers: followers,
+                totalPages: Math.ceil(totalFollowers / showFollowersCount),
+            };
+        },
+    },
+};
